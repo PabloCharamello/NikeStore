265f4e62c63a54bcd63815204a1ad579c10abd1d
Pablo Charamello <pablocharamello@gmail.com>
2022-12-26 14:58:18 -0300

    adding classNames to Hero component h2 divs and buttom

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,12 +6,21 @@ const Hero = ({
   console.log({ socialLinks });
   return (
     <div className="relative h-auto w-auto flex flex-col">
-      <div className="bg-theme clip-path h-[85vh] lg:h-[75vh] md:h-[65vh] sm:h-[55] w-auto absolute top-0 left-0 right-0 opacity-100 z-10"></div>
-      <div className="relative opacity-100 z-20">
-        <div className="">
-          <h1>{title}</h1>
-          <h2>{subtitle}</h2>
-          <button>{btnText}</button>
+      <div className="bg-theme clip-path h-[85vh] lg:h-[75vh] md:h-[65vh] sm:h-[55vh] w-auto absolute top-0 left-0 right-0 opacity-100 z-10"></div>
+      <div className="relative opacity-100 z-20 grid items-center justify-items-center nike-container">
+        <div className="grid items-center justify-items-center mt-28 md:mt-24">
+          <h2 className="text-6xl lg:text-5xl md:text-4xl sm:text-3xl xsm:text-2xl font-extrabold filter drop-shadow-sm text-slate-200">
+            {title}
+          </h2>
+          <h2 className="text-6xl lg:text-5xl md:text-4xl sm:text-3xl xsm:text-2xl font-extrabold filter drop-shadow-sm text-slate-200">
+            {subtitle}
+          </h2>
+          <button
+            type="button"
+            className="button-theme bg-slate-200  shadow-slate-200 rounded-xl my-5"
+          >
+            {btnText}
+          </button>
           {/* <div></div>
           <div></div> */}
         </div>
