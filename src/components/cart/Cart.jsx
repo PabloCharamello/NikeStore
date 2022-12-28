6c78bfa6b7084178903fd8eb2d0af555bf4ba0f2
Pablo Charamello <pablocharamello@gmail.com>
2022-12-28 10:24:55 -0300

    Adding removeItemFromCart and styles on itemComponents in cart

--- src/components/cart/Cart.jsx
+++ src/components/cart/Cart.jsx
@@ -38,10 +38,28 @@ const Cart = () => {
             <CartEmpty />
           ) : (
             <div>
-              <div>
+              <div className="flex items-start justify-start flex-col gap-y-7 lg:gap-y-5 overflow-y-scroll h-[81vh] sroll-smooth scroll-hidden py-3">
                 {cartItems?.map((item, i) => (
                   <CartItem key={i} item={item} />
                 ))}
+              </div>
+              <div className="fixed buttom-0 bg-white w-full px-5 py-2 grid items-center">
+                <div className="flex items-center justify-between">
+                  <h3 className="text-base font-semibold uppercase">
+                    Subtotal
+                  </h3>
+                  <h3 className="text-sm rounded bg-theme-cart text-slate-100 px-1 py-0.5">
+                    000
+                  </h3>
+                </div>
+                <div className="grid items-center gap-2">
+                  <p className="text-sm gfont-medium text-center">
+                    Taxes and shipping will calculate at shipping
+                  </p>
+                  <button className="buttom-theme bg-theme-cart text-white">
+                    Check Out
+                  </button>
+                </div>
               </div>
             </div>
           )}
--- src/components/cart/CartItem.jsx
+++ src/components/cart/CartItem.jsx
@@ -1,9 +1,71 @@
-import React from "react";
+import { useDispatch } from "react-redux";
+import { setRemoveItemFromCart } from "../../app/reducers/CartSlice";
+import { MinusIcon, PlusIcon, TrashIcon } from "@heroicons/react/24/outline";
 
 const CartItem = ({
   item: { id, title, text, img, color, shadow, price, cartQuantity },
 }) => {
-  return <div>CartItem</div>;
+  const onRemoveItem = () => {
+    const dispatch = useDispatch();
+  };
+
+  return (
+    <>
+      <div className="flex items-center justify-between w-full px-5">
+        <div className="flex items-center gap-5">
+          <div
+            className={`bg-gradient-to-b ${color} ${shadow} relative rounded p-3 hover:scale-105 transition-all duration-75 ease-in-out grid items-center`}
+          >
+            <img
+              src={img}
+              alt={`img/cart-item${id}`}
+              className="w-36 h-auto object-fill lg:w-28"
+            />
+          </div>
+          <div className="grid items-center gap-4">
+            <div className="grid items-center leading-none">
+              <h2 className="font-medium text-lg text-slate-900 lg:text-sm">
+                {title}
+              </h2>
+              <p className="text-sm text-slate-800 lg:text-xs">{text}</p>
+            </div>
+            <div className="flex items-center justify-around w-full">
+              <button
+                type="button"
+                className="bg-theme-cart rounded w-6 h-6 lg:w-5 lg:h-5 flex items-center justify-center active:scale-90"
+              >
+                <MinusIcon className="w-5 h-5 lg:w-4 lg:h-4 text-white stroke-[2]" />
+              </button>
+              <div className="bg-theme-cart rounded text-white font-medium lg:text-xs w-7 h-6 lg:h-5 lg:w-6 flex items-center justify-center">
+                {cartQuantity}
+              </div>
+              <button
+                type="button"
+                className="bg-theme-cart rounded w-6 h-6 lg:w-5 lg:h-5 flex items-center justify-center active:scale-90"
+              >
+                <PlusIcon className="w-5 h-5 lg:w-4 lg:h-4 text-white stroke-[2]" />
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className="grid items-center gap-5">
+          <div className="grid items-center justify-items-center">
+            <h3 className="text-lg lg:text-base text-slate-900 font-medium">
+              ${price * cartQuantity}
+            </h3>
+          </div>
+          <div className="">
+            <button
+              className="bg-theme-cart rounded lg:p-0.5 grid items-center justify-items-center"
+              type="button"
+            >
+              <TrashIcon className="w-5 h-5 text-white" />
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default CartItem;
